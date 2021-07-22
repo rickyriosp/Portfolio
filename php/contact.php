6bd1d3aca6093fa84074f7d74d827f16eb3a0301
Ricky Rios <riki.rios91@gmail.com>
2021-07-22 18:34:06 -0400

    updated icons and skill section

--- php/contact.php
+++ php/contact.php
@@ -1,8 +1,5 @@
 <?php
 
-// Email recipient
-$EmailTo = "ricardo@riosr.com";
-
 $errors = "";
 
 // Name
@@ -23,7 +20,7 @@ if (empty($_POST["email"])) {
 if (empty($_POST["subject"])) {
     $errors = "Subject is required ";
 } else {
-    $Subject = $_POST["subject"];
+    $subject = $_POST["subject"];
 }
 
 // Message
@@ -33,26 +30,66 @@ if (empty($_POST["message"])) {
     $message = $_POST["message"];
 }
 
-// Prepare email body text
-$Body = "";
-$Body .= "Name: ";
-$Body .= $name;
-$Body .= "\n";
-$Body .= "Email: ";
-$Body .= $email;
-$Body .= "\n";
-$Body .= "Message: ";
-$Body .= $message;
-$Body .= "\n";
+// Form validation
+if(!empty($name) && !empty($email) && !empty($subject) && !empty($message)){
 
-// Send email
-$success = mail($EmailTo, $Subject, $Body, "From:"."contact@riosr.com");
+    // reCAPTCHA validation
+    if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])) {
 
-// Redirect to success page
-if ($success && $errors == ""){
-   echo 'success';
+        // Google secret API
+        $secretAPIkey = getenv("secretAPIkey");
+
+        // reCAPTCHA response verification
+        $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secretAPIkey.'&response='.$_POST['g-recaptcha-response']);
+
+        // Decode JSON data
+        $response = json_decode($verifyResponse);
+        if($response->success){
+            // Email recipient
+            $toMail = "ricardo@riosr.com";
+
+            // Prepare email body text
+            $Body = "";
+            $Body .= "Name: ";
+            $Body .= $name;
+            $Body .= "\n";
+            $Body .= "Email: ";
+            $Body .= $email;
+            $Body .= "\n";
+            $Body .= "Message: ";
+            $Body .= $message;
+            $Body .= "\n";
+
+            // Send email
+            $success = mail($toMail, $subject, $Body, "From:"."contact@riosr.com");
+
+            // Redirect to success page
+            if ($success && $errors == ""){
+            echo 'success';
+            }
+            else{
+                echo $errors;
+            }
+        } else {
+            $response = array(
+                "status" => "alert-danger",
+                "message" => "Robot verification failed, please try again."
+            );
+            echo implode(", ", $response);
+        }       
+    } else{ 
+        $response = array(
+            "status" => "alert-danger",
+            "message" => "Plese check on the reCAPTCHA box."
+        );
+        echo implode(", ", $response);
+    } 
+}  else{ 
+    $response = array(
+        "status" => "alert-danger",
+        "message" => "All the fields are required."
+    );
+    echo implode(", ", $response);
 }
-else{
-    echo $errors;
-}
+
 ?>
