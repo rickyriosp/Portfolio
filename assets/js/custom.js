7280941de4b50617aa835baecd39681a0c523202
rickyriosp <riki.rios91@gmail.com>
2025-12-10 12:24:23 -0500

    refactor: clean up blog posts and counter section code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,186 +1,199 @@
-/*----------- Blog Posts -----------*/ 
+/*----------- Blog Posts -----------*/
 
-let getBlogPosts = async function(qty) {
-    localUrl = `https://localhost:5001/api/Post?postQty=${qty}`;
-    url = `https://blog.riosr.com/Api/Post?postQty=${qty}`;
-    let response = await fetch(url);
+let getBlogPosts = async function (qty) {
+  localUrl = `https://localhost:5001/api/Post?postQty=${qty}`;
+  url = `https://blog.riosr.com/Api/Post?postQty=${qty}`;
+  let response = await fetch(url);
 
-    if (response.ok) { // if HTTP-status is 200-299
-        // get the response body (the method explained below)
-        let json = await response.json();
+  if (response.ok) {
+    // if HTTP-status is 200-299
+    // get the response body (the method explained below)
+    let json = await response.json();
 
-        let container = document.getElementById("blog-posts-container");
-        json.forEach(element => {
-            let htmlString =
-                `
+    let container = document.getElementById('blog-posts-container');
+    json.forEach((element) => {
+      let htmlString = `
                 <!-- col -->
                 <div class="col-md-6 col-xl-4 mb-5">
                     <!-- card -->
                     <div class="card blog-post-card">
-                        <img class="card-img-top" src="data:${element.contentType};base64,${element.imageData}">
+                        <img class="card-img-top" src="data:${element.contentType};base64,${
+        element.imageData
+      }">
                         <div class="card-body">
-                            <h5 class="card-title"><a class="theme-link" href="https://blog.riosr.com/BlogPosts/UrlFriendly/${element.slug}" target="_blank">${element.title}</a></h5>
+                            <h5 class="card-title"><a class="theme-link" href="https://blog.riosr.com/BlogPosts/UrlFriendly/${
+                              element.slug
+                            }" target="_blank">${element.title}</a></h5>
                             <p class="card-text">${element.abstract}</p>
-                            <p class="mb-0"><a class="text-link" href="https://blog.riosr.com/BlogPosts/UrlFriendly/${element.slug}" target="_blank">Read more &rarr;</a></p>
+                            <p class="mb-0"><a class="text-link" href="https://blog.riosr.com/BlogPosts/UrlFriendly/${
+                              element.slug
+                            }" target="_blank">Read more &rarr;</a></p>
                         </div>
                         <div class="card-footer">
-                            <small class="text-muted">Published on ${(new Date(element.created)).toLocaleDateString('en-US')}</small>
+                            <small class="text-muted">Published on ${new Date(
+                              element.created,
+                            ).toLocaleDateString('en-US')}</small>
                         </div>
                     </div>
                 </div>
                 `;
 
-            container.append(stringToHTML(htmlString));
-        });
-    } else {
-        alert("HTTP-Error: " + response.status);
-    }
-}
+      container.append(stringToHTML(htmlString));
+    });
+  } else {
+    alert('HTTP-Error: ' + response.status);
+  }
+};
 
-let stringToHTML = function(str) {
-    var parser = new DOMParser();
-    var doc = parser.parseFromString(str, 'text/html');
+let stringToHTML = function (str) {
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(str, 'text/html');
 
-    return doc.getElementsByClassName('col-md-6')[0];
+  return doc.getElementsByClassName('col-md-6')[0];
 };
 
 getBlogPosts(3);
 
-
 /*----------- Contact -----------*/
 
-$('.contact-form').on('submit', function(event) {
-    var form = $(this);
-    var submitBtn = form.find('#contact-submit');
-    var submitBtnText = submitBtn.text();
-    var feedbackEl = form.find('.contact-feedback');
-    event.preventDefault();
-    // Waiting for the response from the server
-    submitBtn.html('Wait...').addClass('wait').prop('disabled', true);
-    setTimeout(function() {
-        // Posts the Form's data to the server using Ajax
-        $.ajax({
-                url: form.attr('action'),
-                type: 'POST',
-                data: form.serialize(),
-            })
-            // Getting a response from the server
-            .done(function(response) {
-                // If the PHP file succeed sending the message
-                if (response == 'success') {
-                    // Feedback to the user
-                    submitBtn.removeClass('wait').html('Success').addClass('success');
-                    feedbackEl.addClass('success').html('Thank you, your message has been sent.').fadeIn(200);
-                    setTimeout(function() {
-                        submitBtn.html(submitBtnText).removeClass('success').prop('disabled', false);
-                        feedbackEl.fadeOut(200).removeClass('success').html('');
-                    }, 6000);
-                    // Clears the Form
-                    form[0].reset();
-                    // If something is wrong
-                } else {
-                    // Feedback to the user
-                    console.log(response);
-                    submitBtn.removeClass('wait').html('Error').addClass('error');
-                    feedbackEl.addClass('error').html('Server error! Please check your browser console log for more details.').fadeIn(200);
-                    setTimeout(function() {
-                        submitBtn.html(submitBtnText).removeClass('error').prop('disabled', false);
-                        feedbackEl.fadeOut(200).removeClass('error').html('');
-                    }, 6000);
-                }
-            });
-    }, 1000);
+$('.contact-form').on('submit', function (event) {
+  var form = $(this);
+  var submitBtn = form.find('#contact-submit');
+  var submitBtnText = submitBtn.text();
+  var feedbackEl = form.find('.contact-feedback');
+  event.preventDefault();
+  // Waiting for the response from the server
+  submitBtn.html('Wait...').addClass('wait').prop('disabled', true);
+  setTimeout(function () {
+    // Posts the Form's data to the server using Ajax
+    $.ajax({
+      url: form.attr('action'),
+      type: 'POST',
+      data: form.serialize(),
+    })
+      // Getting a response from the server
+      .done(function (response) {
+        // If the PHP file succeed sending the message
+        if (response == 'success') {
+          // Feedback to the user
+          submitBtn.removeClass('wait').html('Success').addClass('success');
+          feedbackEl.addClass('success').html('Thank you, your message has been sent.').fadeIn(200);
+          setTimeout(function () {
+            submitBtn.html(submitBtnText).removeClass('success').prop('disabled', false);
+            feedbackEl.fadeOut(200).removeClass('success').html('');
+          }, 6000);
+          // Clears the Form
+          form[0].reset();
+          // If something is wrong
+        } else {
+          // Feedback to the user
+          console.log(response);
+          submitBtn.removeClass('wait').html('Error').addClass('error');
+          feedbackEl
+            .addClass('error')
+            .html('Server error! Please check your browser console log for more details.')
+            .fadeIn(200);
+          setTimeout(function () {
+            submitBtn.html(submitBtnText).removeClass('error').prop('disabled', false);
+            feedbackEl.fadeOut(200).removeClass('error').html('');
+          }, 6000);
+        }
+      });
+  }, 1000);
 });
-
 
 /*----------- View Resume Button -----------*/
 
-document.getElementById("viewResumeButton").addEventListener("click", function () {
-    // Select the hidden resume section
-    const resumeHidden = document.getElementById("resume-hidden");
-    // Select all elements under .main-wrapper that do not have the .no-hide class
-    const elementsToHide = document.querySelectorAll('.main-wrapper > :not(.no-hide)');
+document.getElementById('viewResumeButton').addEventListener('click', function () {
+  // Select the hidden resume section
+  const resumeHidden = document.getElementById('resume-hidden');
+  // Select all elements under .main-wrapper that do not have the .no-hide class
+  const elementsToHide = document.querySelectorAll('.main-wrapper > :not(.no-hide)');
 
-    // Hide the other sections
-    elementsToHide.forEach(element => {
-        element.style.display = "none";
-    });
-    // Make the hidden resume section visible
-    resumeHidden.style.display = "block";
+  // Hide the other sections
+  elementsToHide.forEach((element) => {
+    element.style.display = 'none';
+  });
+  // Make the hidden resume section visible
+  resumeHidden.style.display = 'block';
 
-    // Redirect to index.html with the target section
-    window.location.href = `#resume`;
+  // Redirect to index.html with the target section
+  window.location.href = `#resume`;
 
-    // Scroll to the top of the resume section
-    window.scrollTo({
-        top: resumeHidden.offsetTop,
-        behavior: "smooth"
-    });
+  // Scroll to the top of the resume section
+  window.scrollTo({
+    top: resumeHidden.offsetTop,
+    behavior: 'smooth',
+  });
 });
-
 
 /*----------- Navbar Link Handling on Resume Page -----------*/
 
-document.querySelectorAll('.navbar-nav a').forEach(link => {
-    link.addEventListener('click', function (event) {
-        // Check if the current URL is index.html#resume
-        if (window.location.href.includes('resume')) {
-            // Prevent default navigation behavior
-            event.preventDefault();
+document.querySelectorAll('.navbar-nav a').forEach((link) => {
+  link.addEventListener('click', function (event) {
+    // Check if the current URL is index.html#resume
+    if (window.location.href.includes('resume')) {
+      // Prevent default navigation behavior
+      event.preventDefault();
 
-            // Get the target section from the clicked link's href
-            const targetSection = link.getAttribute('href');
+      // Get the target section from the clicked link's href
+      const targetSection = link.getAttribute('href');
 
-            // Select the hidden resume section
-            const resumeHidden = document.getElementById("resume-hidden");
-            // Select all elements under .main-wrapper that do not have the .no-hide class
-            const elementsToHide = document.querySelectorAll('.main-wrapper > :not(.no-hide)');
+      // Select the hidden resume section
+      const resumeHidden = document.getElementById('resume-hidden');
+      // Select all elements under .main-wrapper that do not have the .no-hide class
+      const elementsToHide = document.querySelectorAll('.main-wrapper > :not(.no-hide)');
 
-            // Hide the other main-wrapper
-            elementsToHide.forEach(element => {
-                element.style.display = "block";
-            });
-            // Make the hidden resume section visible
-            resumeHidden.style.display = "none";
+      // Hide the other main-wrapper
+      elementsToHide.forEach((element) => {
+        element.style.display = 'block';
+      });
+      // Make the hidden resume section visible
+      resumeHidden.style.display = 'none';
 
-            // Redirect to index.html with the target section
-            window.location.href = `${targetSection}`;
-        }
-    });
+      // Redirect to index.html with the target section
+      window.location.href = `${targetSection}`;
+    }
+  });
 });
-
 
 /*----------- Counter API endpoint -----------*/
-const counterApiUrl = 'https://resume.riosr.com/counter';
+// const counterApiUrl = 'https://resume.riosr.com/api/counter';
+// const counterApiUrl = window.counterApiUrl;
 
 // Get button handler
-document.getElementById('get-counter-btn').addEventListener('click', async function() {
-    try {
-        const response = await fetch(counterApiUrl);
-        if (response.ok) {
-            const data = await response.json();
-            document.getElementById('counter-value').textContent = data.value;
-        } else {
-            console.log('Error fetching counter value');
-        }
-    } catch (error) {
-        console.log('Network error');
-    }
-});
+// document.getElementById('get-counter-btn').addEventListener('click', async function () {
+//   try {
+//     const response = await fetch(counterApiUrl);
+//     if (response.ok) {
+//       const data = await response.json();
+//       document.getElementById('counter-value').textContent = data.value;
+//     } else {
+//       console.log('Error fetching counter value');
+//     }
+//   } catch (error) {
+//     console.log('Network error');
+//   }
+// });
 
 // Increment button handler
-document.getElementById('increment-counter-btn').addEventListener('click', async function() {
-    try {
-        const response = await fetch(counterApiUrl, {
-            method: 'POST'
-        });
-        if (response.ok) {
-            const data = await response.json();
-            document.getElementById('counter-value').textContent = data.value;
-        } else {
-            console.log('Error incrementing counter');
-        }
-    } catch (error) {
-        console.log('Network error');
-    }
-});
+// document.getElementById('increment-counter-btn').addEventListener('click', async function () {
+//   try {
+//     console.log('Incrementing counter at', counterApiUrl);
+
+//     const response = await fetch(counterApiUrl, {
+//       method: 'POST',
+//     });
+//     if (response.ok) {
+//       const data = await response.json();
+//       // Update all elements with id 'counter-value'
+//       document.querySelectorAll('#counter-value').forEach((el) => {
+//         el.textContent = data.value;
+//       });
+//     } else {
+//       console.log('Error incrementing counter');
+//     }
+//   } catch (error) {
+//     console.log('Network error');
+//   }
+// });
